c1ea43f496e5508353722b729e0080eb60069f14
abinashpun <ap563313@ohio.edu>
2022-08-26 14:35:34 -0500

    output format consistent with uiuc group

--- supplement_codes/src/FinalTree.h
+++ supplement_codes/src/FinalTree.h
@@ -14,16 +14,35 @@ class FinalTree
  protected:
 
 /// Run and event level info
-  int runN, spillID, eventID, fpga1, occuD1,  occuD2,  occuD3m,occuD3p, TargetPos;
+  int runN, runID, spillID, eventID, fpga1, occuD1,  occuD2,  occuD3m,occuD3p, targetPos, TargetPos;
   float rfp00c, pot_p00, liveP;
 
-  float true_dim_mass, true_dim_pT, true_dim_xF,true_dim_xT, weight;
+  float true_mass, true_pT, true_xF,true_xT, weight, true_costh,true_phi;
+
+  float px1, py1, pz1, px2, py2, pz2;
+  float trackSeparation;
+
+  float px1_st1, py1_st1, pz1_st1, px2_st1, py2_st1, pz2_st1;
+  float px1_st3, py1_st3, pz1_st3, px2_st3, py2_st3, pz2_st3;
+  int D1, D2, D3;
+
+  int nHits1St1, nHits1St2, nHits1St3;
+  int nHits2St1, nHits2St2, nHits2St3;
+
+  float  x1_t, y1_t,  x1_d, y1_d, x1_v, y1_v, z1_v; 
+  double x1_st1, x1_st3, x2_st1, x2_st3;
+  double y1_st1, y1_st3, y2_st1, y2_st3;
+  float  x2_t, y2_t,  x2_d, y2_d,x2_v, y2_v,z2_v;
+  float chisq1, chisq1_upstream, chisq1_target,chisq1_dump;
+  float chisq2, chisq2_upstream, chisq2_target,chisq2_dump;
+  int nHits1, nHits2;
 
 
-  float dim_mass, dim_costh, dim_phi, dim_xF, dim_xT, dim_xB, dim_pT,
-    dim_vtx_x, dim_vtx_y, dim_vtx_z;
-  float dim_px, dim_py, dim_pz;
-  float dim_chisq_target, dim_chisq_dump, dim_chisq_vtx, dim_chisq_upstream;
+  float mass, costh, phi, xF, xT, xB, pT,  vtx_x, vtx_y, vtx_z;
+  float dpx, dpy, dpz, dim_mup_px,dim_mup_py,dim_mup_pz,dim_mum_px,dim_mum_py,dim_mum_pz;
+  float chisq_target, chisq_dump, chisq_dimuon, chisq_upstream;
+
+  float dx, dy, dz;
 
 
 
@@ -40,8 +59,7 @@ class FinalTree
  double ptrk_x_st3,   ptrk_y_st3; //position and momentum at st3
  double ptrk_px_st3, ptrk_py_st3, ptrk_pz_st3;
  int  ptrk_nHits;// ptrk_nHits_St1, ptrk_nHits_St2,  ptrk_nHits_St3, ptrk_nHits_St4H, ptrk_nHits_St4V; //number of hits at various stations
- //double ptrk_z0x; //?
- //int ptrk_charge,  ptrk_Road;
+ 
 
 
 
@@ -51,14 +69,14 @@ class FinalTree
  float ntrk_x_d, ntrk_y_d; /// position at dump
  float ntrk_x_t, ntrk_y_t; /// position at target
  float ntrk_vtx_z;/// position and momentum at vertex
- //double ntrk_px_v,  ntrk_py_v,  ntrk_pz_v;
+
+
  double ntrk_x_st1, ntrk_y_st1; //position and momentum at st1
  double ntrk_px_st1,  ntrk_py_st1,  ntrk_pz_st1;
  double ntrk_x_st3,   ntrk_y_st3; //position and momentum at st3
  double ntrk_px_st3, ntrk_py_st3, ntrk_pz_st3;
  int  ntrk_nHits;//, ntrk_nHits_St1, ntrk_nHits_St2,  ntrk_nHits_St3, ntrk_nHits_St4H, ntrk_nHits_St4V; //number of hits at various stations
- //double ntrk_z0x; //?
- //int ntrk_charge,  ntrk_Road;
+ 
 
 
 };
